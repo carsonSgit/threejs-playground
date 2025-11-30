e30e13df66150326dc7423c32b8a5e0e14692d6e
Carson <92652800+carsonSgit@users.noreply.github.com>
2025-11-30 17:48:06 -0500

    clean unused files

--- core/src/actions/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * TODO: Add your actions here
- *
- * This is a placeholder file to initialize the actions directory.
- * You can delete this file once you add your own actions.
- */
-
-export default {};
